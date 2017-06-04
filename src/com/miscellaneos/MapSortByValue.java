3013467a8882680b241a58f64572cee4f8c14908
Anuja Asalkar <anujaasalkar@gmail.com>
2017-06-03 20:22:52 -0700

    Code for sorting Map based on value
    Code for sorting Map based on value

---
+++ src/com/miscellaneos/MapSortByValue.java
@@ -0,0 +1,46 @@
+package com.miscellaneos;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+public class MapSortByValue {
+
+	//Generic Comparator
+	static class ValueComparator<K, V extends Comparable<V>> implements Comparator<K> {
+
+		HashMap<K, V> hmap = new HashMap<K, V>();
+
+		public ValueComparator(HashMap<K, V> map) {
+			hmap.putAll(map);
+		}
+
+		public int compare(K k1, K k2) {
+			return hmap.get(k1).compareTo(hmap.get(k2));
+		}
+
+	}
+
+	public static TreeMap<String, Integer> sortByValue(HashMap<String, Integer> hmap) {
+
+		Comparator<String> vc = new ValueComparator<String, Integer>(hmap);
+
+		TreeMap<String, Integer> tm = new TreeMap<>(vc);
+		tm.putAll(hmap);
+
+		return tm;
+	}
+
+	public static void main(String[] args) {
+		HashMap<String, Integer> map = new HashMap<>();
+		map.put("a", 34);
+		map.put("b", 23);
+		map.put("c", 15);
+		map.put("d", 111);
+		System.out.println(map);
+
+		TreeMap<String, Integer> tmap = sortByValue(map);
+		System.out.println(tmap);
+	}
+
+}
